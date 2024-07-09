28d13a770328866dff71d8e52be8637e8af8f206
michalrzak <michal.r.zak@protonmail.com>
2024-07-09 16:50:55 +0200

    now with tagger saving

--- auditory_intention_decoding_data_analysis/models/build_samples.py
+++ auditory_intention_decoding_data_analysis/models/build_samples.py
@@ -1,7 +1,7 @@
 import csv
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Dict
 
 import mne
 import numpy as np
@@ -53,18 +53,22 @@ def main(
     events, event_id = mne.events_from_annotations(raw)
     epochs = mne.Epochs(raw, events, event_id, tmin=tmin, tmax=tmax, baseline=(None, 0), event_repeated="drop")
 
-    epochs_options: mne.Epochs = epochs[[f"Stimulus/S {triggers.option}" for triggers in TRIGGERS.values()]]
-    epochs_targets: mne.Epochs = epochs[[f"Stimulus/S {triggers.target}" for triggers in TRIGGERS.values()]]
+    epochs_options: Dict[str, mne.Epochs] = {trigger: epochs[f"Stimulus/S {TRIGGERS[trigger].option}"] for trigger in TRIGGERS}
+    epochs_targets: Dict[str, mne.Epochs] = {trigger: epochs[f"Stimulus/S {TRIGGERS[trigger].target}"] for trigger in TRIGGERS}
 
-    epochs_combined = mne.epochs.concatenate_epochs([epochs_options, epochs_targets])
-    labels = np.array([0] * len(epochs_options) + [1] * len(epochs_targets))
+    epochs_combined = mne.epochs.concatenate_epochs([*epochs_options.values(), *epochs_targets.values()])
+    labels = np.array([0] * sum([len(eo) for eo in epochs_options.values()]) +
+                      [1] * sum([len(et) for et in epochs_targets.values()]))
+    taggers = (sum([[tagger] * len(epochs_options[tagger]) for tagger in epochs_options.keys()], []) +
+               sum([[tagger] * len(epochs_targets[tagger]) for tagger in epochs_targets.keys()], []))
+
 
     logger.info("Saving epochs")
     epochs_combined.save(output_file, overwrite=True)
     with open(file_to_label_file(output_file), "w") as file:
         writer = csv.writer(file, lineterminator="\n")
         writer.writerow(["Target", "Tagger"])
-        writer.writerows([[int(ele), ] for ele in labels])
+        writer.writerows([[int(label), tagger] for label, tagger in zip(labels, taggers)])
 
 
 if __name__ == '__main__':
--- auditory_intention_decoding_data_analysis/models/eegnet2.py
+++ auditory_intention_decoding_data_analysis/models/eegnet2.py
@@ -1,4 +1,5 @@
 import pickle
+from dataclasses import dataclass
 from pathlib import Path
 from typing import List
 
@@ -21,13 +22,22 @@ mne.use_log_level("warning")
 app = typer.Typer()
 
 
-def read_labels_file(labels_file: Path) -> List[int]:
-    with open(labels_file, "r") as file:
-        headers = file.readline()
-        assert headers.strip() == "Label"
+@dataclass
+class Prediction:
+    predictions: np.ndarray
+    labels: np.ndarray
+    taggers: List[str]
 
-        return [int(line.strip()) for line in file.readlines()]
 
+def eegnet_reshape(data: np.ndarray) -> np.ndarray:
+    return data.reshape((data.shape[0], data.shape[1], data.shape[2], 1))
+
+
+def read_labels_file(labels_file: Path) -> pd.DataFrame:
+    df = pd.read_csv(labels_file)
+    assert "Target" in df.columns and "Tagger" in df.columns
+
+    return df
 
 @app.command()
 def main(
@@ -43,43 +53,50 @@ def main(
     assert all(epoch.info["sfreq"] == 128 for epoch in epochs)
 
     epochs_combined: mne.Epochs = mne.concatenate_epochs(epochs)
-    labels_combined = np.array(sum([read_labels_file(file) for file in labels_files], []))
-    labels_onehot = np.stack([-labels_combined + 1, labels_combined], axis=1)
+    labels_combined_df = pd.concat([read_labels_file(file) for file in labels_files])
+    labels_onehot = np.stack([-labels_combined_df["Target"] + 1, labels_combined_df["Target"]], axis=1)
+    taggers = np.array(labels_combined_df["Tagger"])
 
     data = epochs_combined.get_data()
 
-    scaler = mne.decoding.Scaler(scalings="median")
-    data_scaled = scaler.fit_transform(data)
+    sss = StratifiedShuffleSplit(n_splits=10, test_size=0.2, random_state=42)
+    sss.get_n_splits(data, labels_onehot)
 
-    data_shaped = data_scaled.reshape((data_scaled.shape[0], data_scaled.shape[1], data_scaled.shape[2], 1))
-
-    sss = StratifiedShuffleSplit(n_splits=10, test_size=0.2, random_state=45)
-    sss.get_n_splits(data_shaped, labels_onehot)
-
-    splits = sss.split(data_shaped, labels_onehot)
+    splits = sss.split(data, labels_onehot)
 
     histories = []
     accuracies = []
     f1_scores = []
+    predictions = []
     for i, split in enumerate(splits):
 
-        data_train = data_shaped[split[0]]
+        data_train = data[split[0]]
         labels_train = labels_onehot[split[0]]
 
-        data_test = data_shaped[split[1]]
+        data_test = data[split[1]]
         labels_test = labels_onehot[split[1]]
+        taggers_test = taggers[split[1]]
+
+        scaler = mne.decoding.Scaler(scalings="median")
+        data_train_scaled = scaler.fit_transform(data_train)
+        data_test_scaled = scaler.transform(data_test)
+
+        data_train_scaled_shaped = eegnet_reshape(data_train_scaled)
+        data_test_scaled_shaped = eegnet_reshape(data_test_scaled)
 
         model: Model = EEGNet(2, 126, int(3.5 * 128 + 1))  # TODO: parameters
+        model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.001), metrics=['accuracy'])
 
-        model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.001), metrics=['accuracy']) # binary_crossentropy
-
-        history = model.fit(data_train, labels_train, epochs=500, batch_size=32, verbose=1)
+        history = model.fit(data_train_scaled_shaped, labels_train, epochs=500, batch_size=32, verbose=1)
         histories.append(history.history)
 
         argmax_labels = np.argmax(labels_test, axis=1)
-        argmax_predictions = np.argmax(model.predict(data_test), axis=1)
+        argmax_predictions = np.argmax(model.predict(data_test_scaled_shaped), axis=1)
         accuracies.append(metrics.accuracy_score(argmax_labels, argmax_predictions))
         f1_scores.append(metrics.f1_score(argmax_labels, argmax_predictions, average='weighted'))
+
+        prediction = Prediction(predictions=argmax_predictions, labels=argmax_labels, taggers=taggers_test)
+        predictions.append(prediction)
 
         model.save(output_folder / f"model-split-{i}.keras")
 
@@ -91,6 +108,9 @@ def main(
 
     with open(output_folder / "f1.pkl", "wb") as file:
         pickle.dump(f1_scores, file)
+
+    with open(output_folder / "prediction.pkl", "wb") as file:
+        pickle.dump(predictions, file)
 
     history_accuracies = pd.DataFrame([history["accuracy"] for history in histories]).melt()
     history_loss = pd.DataFrame([history["loss"] for history in histories]).melt()
